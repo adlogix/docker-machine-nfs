fb20ef9cfa596aafef005ccde92349d369335861
Wei Kin Huang <weikin.huang04@gmail.com>
2019-01-16 15:14:32 -0500

    Support docker-machine-driver-vmware and use machine ip for route

--- docker-machine-nfs.sh
+++ docker-machine-nfs.sh
@@ -266,9 +266,9 @@ lookupMandatoryProperties ()
 
   prop_machine_driver=$(getMachineDriver $1 "$2")
 
-  if [ "$prop_machine_driver" = "vmwarefusion" ]; then
+  if [ "$prop_machine_driver" = "vmwarefusion" ] || [ "$prop_machine_driver" = "vmware" ]; then
     prop_network_id="Shared"
-    prop_nfshost_ip=${prop_use_ip:-"$(ifconfig -m `route get 8.8.8.8 | awk '{if ($1 ~ /interface:/){print $2}}'` | awk 'sub(/inet /,""){print $1}')"}
+    prop_nfshost_ip=${prop_use_ip:-"$(ifconfig -m `route get $prop_machine_ip | awk '{if ($1 ~ /interface:/){print $2}}'` | awk 'sub(/inet /,""){print $1}')"}
     prop_machine_ip=$prop_nfshost_ip
     if [ "" = "${prop_nfshost_ip}" ]; then
       echoError "Could not find the vmware fusion net IP!"; exit 1
